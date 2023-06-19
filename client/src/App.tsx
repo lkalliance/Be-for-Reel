910b1c22927be27e00aea4238eb2d669a434cf43
lkalliance <119076402+lkalliance@users.noreply.github.com>
2023-06-19 13:19:27 -0500

    Removed unused imports from App page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Routes, Route, Link, useLocation, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import { Home, Profile, Poll, Create, Directory } from "./pages";
 import { Header } from "./components";
