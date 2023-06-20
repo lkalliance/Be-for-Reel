74683972e191e7fd79b132a31e84695345b35715
lkalliance <119076402+lkalliance@users.noreply.github.com>
2023-06-20 15:23:26 -0500

    Continued basic setup of login form

--- client/src/components/LoginForm/LoginForm.tsx
+++ client/src/components/LoginForm/LoginForm.tsx
@@ -1,21 +1,37 @@
 import "./LoginForm.css";
 import { useState } from "react";
-import { Form, Button } from "react-bootstrap";
+// import { Form, Button } from "react-bootstrap";
 
 export function LoginForm() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = e.target;
+    return id === "username" ? setUsername(value) : setPassword(value);
+  };
+  const handleSubmit = (e: React.MouseEvent) => {
+    e.preventDefault();
+    console.log(`Username ${username} and password ${password}`);
+  };
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="loginForm.usernameInput">
-        <Form.Label>username</Form.Label>
-        <Form.Control type="text" placeholder="username" />
-      </Form.Group>
-      <Form.Group controlId="loginForm.passwordInput">
-        <Form.Label>password</Form.Label>
-        <Form.Control type="password" placeholder="password" />
-      </Form.Group>
-      <Button>Submit</Button>
-    </Form>
+    <form>
+      <label>username</label>
+      <input
+        type="text"
+        placeholder="username"
+        id="username"
+        onChange={handleInputChange}
+      />
+      <label>password</label>
+      <input
+        type="password"
+        placeholder="password"
+        id="password"
+        onChange={handleInputChange}
+      />
+      <button type="submit" onClick={handleSubmit}>
+        Submit
+      </button>
+    </form>
   );
 }
