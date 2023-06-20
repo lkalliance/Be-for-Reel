4b3715763013e13b884cf6c731713beb8a46a20f
lkalliance <119076402+lkalliance@users.noreply.github.com>
2023-06-20 10:45:58 -0500

    switched Apollo to localhost

--- server/config/connection.tsx
+++ server/config/connection.tsx
@@ -1,13 +1,17 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const url = `mongodb+srv://lkalliance:${process.env.DB_PASSWORD}@lkalliance.jkjosg0.mongodb.net/shelf_life_db?retryWrites=true&w=majority`;
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/be_for_reel_db"
+);
 
-mongoose.connect(url, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+// const url = `mongodb+srv://lkalliance:${process.env.DB_PASSWORD}@lkalliance.jkjosg0.mongodb.net/shelf_life_db?retryWrites=true&w=majority`;
+
+// mongoose.connect(url, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false,
+// });
 
 module.exports = mongoose.connection;
