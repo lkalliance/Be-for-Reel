e9cefcc9ba89ab2bb8c453db18f9282817849754
lkalliance <119076402+lkalliance@users.noreply.github.com>
2023-08-02 10:27:37 -0500

    Fixed bug with mutation definition

--- client/src/utils/mutations.tsx
+++ client/src/utils/mutations.tsx
@@ -23,8 +23,20 @@ export const ADD_USER = gql`
 `;
 
 export const ADD_POLL = gql`
-  mutation AddPoll($userId: String!, $title: String!, $movieIds: [String]!, $userName: String, description: String) {
-    addPoll(userId: $userId, title: $title, movieIds: $movieIds, userName: $userName, description: $description) {
+  mutation AddPoll(
+    $userId: String!
+    $title: String!
+    $movieIds: [String]!
+    $userName: String
+    $description: String
+  ) {
+    addPoll(
+      userId: $userId
+      title: $title
+      movieIds: $movieIds
+      userName: $userName
+      description: $description
+    ) {
       poll_id
     }
   }
