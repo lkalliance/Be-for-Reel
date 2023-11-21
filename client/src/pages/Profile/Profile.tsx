fa4f12b6f734cf0cd2d66f4d84ebd2b55ad215a8
lkalliance <119076402+lkalliance@users.noreply.github.com>
2023-11-21 10:43:45 -0600

    styled user profile page

--- client/src/pages/Profile/Profile.tsx
+++ client/src/pages/Profile/Profile.tsx
@@ -20,32 +20,45 @@ export function Profile() {
   const createdOn = new Date(userData.created);
 
   return (
-    <section id="profile">
-      {userData.created ? (
-        // the user exists, render their data
-        <>
-          <h1>{userData.userName}</h1>
-          <h4>{`member since ${createdOn.getFullYear()}`}</h4>
-          {userData ? <PollList polls={userData.polls} /> : <div></div>}
-          <h2>Comments</h2>
-          {userData.comments.map((comment: userCommentProps, index: Key) => {
-            return (
-              <div key={index}>
-                <h3>
-                  poll: <Link to={comment.urlTitle}>{comment.title}</Link>
-                </h3>
-                <h4>{`Your vote: "${comment.movie}"`}</h4>
-                <p>{comment.text}</p>
+    <section id="profile" className="container">
+      <div className="row">
+        {userData.created ? (
+          // the user exists, render their data
+          <>
+            <h1 className="col col-12">{userData.userName}</h1>
+            <h4 className="col col-12">{`member since ${createdOn.getFullYear()}`}</h4>
+
+            {userData ? (
+              <div className="col col-12 col-sm-6">
+                <PollList polls={userData.polls} />
               </div>
-            );
-          })}
-        </>
-      ) : (
-        // the user doesn't exist, tell the user so
-        <div>
-          The user <span>{username}</span> does not exist.
-        </div>
-      )}
+            ) : (
+              ""
+            )}
+            <div className="col col-12 col-sm-6">
+              <h2>Comments</h2>
+              {userData.comments.map(
+                (comment: userCommentProps, index: Key) => {
+                  return (
+                    <div className="comment" key={index}>
+                      <h5>
+                        <Link to={comment.urlTitle}>{comment.title}</Link>
+                      </h5>
+                      <p className="your-vote">{`Your vote: "${comment.movie}"`}</p>
+                      <p>{comment.text}</p>
+                    </div>
+                  );
+                }
+              )}
+            </div>
+          </>
+        ) : (
+          // the user doesn't exist, tell the user so
+          <div>
+            The user <span>{username}</span> does not exist.
+          </div>
+        )}
+      </div>
     </section>
   );
 }
