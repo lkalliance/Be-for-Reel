b2eea22d33d62ccea205332fa6b6cb0c7427635e
lkalliance <119076402+lkalliance@users.noreply.github.com>
2023-12-05 15:38:58 -0600

    added footer

--- client/src/pageComponents/Footer/Footer.tsx
+++ client/src/pageComponents/Footer/Footer.tsx
@@ -1,8 +1,16 @@
 // This component renders the footer for all pages
 
 import "./Footer.css";
+import logo from "./imdb-icon-20.png";
 
 export function Footer() {
   // footer code
-  return <footer></footer>;
+  return (
+    <footer>
+      <span>Be real about your favorite reels. </span>Powered by{" "}
+      <a href="http://imdb.com" target="_blank" rel="noreferrer">
+        <img src={logo} alt="IMDb" />
+      </a>
+    </footer>
+  );
 }
