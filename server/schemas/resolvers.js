2ee17fd8fde43344ca82e78db0890a89082ef66d
lkalliance <119076402+lkalliance@users.noreply.github.com>
2024-01-18 16:30:59 -0600

    Renaming urlTitle on deactivation to make old urlTitle available again

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -555,17 +555,18 @@ const resolvers = {
     deactivatePoll: async (parent, { poll_id }, context) => {
       if (context.user) {
         try {
+          const deactivation = `deactivated-${poll_id}`;
           // update the given poll to be deactivated
           const pollUpdate = await Poll.findOneAndUpdate(
             { _id: poll_id },
-            { deactivated: true },
+            { deactivated: true, urlTitle: deactivation },
             { new: true, useFindAndModity: false }
           );
 
           // update the user's poll list
           await User.findOneAndUpdate(
             { lookupName: context.user.lookupName, "polls.poll_id": poll_id },
-            { "polls.$.deactivated": true },
+            { "polls.$.deactivated": true, "polls.$.urlTitle": deactivation },
             { new: true, useFindAndModify: false }
           );
 
@@ -574,7 +575,10 @@ const resolvers = {
             if (genre !== "all") {
               const genreUpdate = await Genre.findOneAndUpdate(
                 { title: genre, "polls.poll_id": poll_id },
-                { "polls.$.deactivated": true },
+                {
+                  "polls.$.deactivated": true,
+                  "polls.$.urlTitle": deactivation,
+                },
                 { new: true, useFindAndModify: false, upsert: false }
               );
             }
