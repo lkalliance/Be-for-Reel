1c82b11ec6bf1afe6489db3e708d422f1ce77e35
lkalliance <119076402+lkalliance@users.noreply.github.com>
2024-02-05 11:06:58 -0600

    Adjusted treatment of home page title

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -12,15 +12,13 @@ export function Home() {
   return (
     <section id="home" className="container">
       <h1 className="homepage-title">Be for Reel</h1>
-      <div className="lead">
-        <p className="lead">
-          Be real about your favorite reels.
-          <span className="extra">
-            Participate in polls about your favorite movies. Create, vote, and
-            comment!
-          </span>
-        </p>
-      </div>
+      <p className="lead">
+        Be real about your favorite reels.
+        <span className="extra">
+          Participate in polls about your favorite movies. Create, vote, and
+          comment!
+        </span>
+      </p>
       <div className="row row-cols-1 row-cols-md-3 g-3 justify-content-center">
         {loading ? (
           <div className="doesnt-exist">loading...</div>
